5bd08fe7e866a1fcad8b49cb4f246ac3c85715ea
Evan Wieland <evwiel1@gmail.com>
2022-12-22 11:00:38 -0600

    Chapter 11: Testing - Complete

--- mirage/config.js
+++ mirage/config.js
@@ -1,4 +1,10 @@
 export default function () {
   this.get('/bands');
   this.get('/bands/:id');
+  this.get('/bands/:id/songs', (schema, request) => {
+    const { id } = request.params;
+    return schema.songs.where({ bandId: id });
+  });
+
+  this.post('/bands');
 }
--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,8 +1,9 @@
 import { module, test } from 'qunit';
-import { visit } from '@ember/test-helpers';
+import { visit, waitFor } from '@ember/test-helpers';
 import { setupApplicationTest } from 'rarwe/tests/helpers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { getPageTitle } from 'ember-page-title/test-support';
+import { createBand } from 'rarwe/tests/helpers/custom-helpers';
 
 module('Acceptance | bands', function (hooks) {
   setupApplicationTest(hooks);
@@ -15,16 +16,37 @@ module('Acceptance | bands', function (hooks) {
     await visit('/');
 
     assert.strictEqual(getPageTitle(), 'Bands | Rock & Roll with Octane');
-
-    let bandLinks = document.querySelectorAll('.mb-2 > a');
-    assert.strictEqual(bandLinks.length, 2, 'All band links are rendered');
-    assert.ok(
-      bandLinks[0].textContent.includes('Radiohead'),
-      'First band link contains the band name'
-    );
-    assert.ok(
-      bandLinks[1].textContent.includes('Long Distance Calling'),
-      'The other band link contains the band name'
-    );
+    assert
+      .dom('[data-test-rr="band-link"]')
+      .exists({ count: 2 }, 'All band links are rendered');
+    assert
+      .dom('[data-test-rr="band-list-item"]:first-child')
+      .hasText('Radiohead', 'First band link contains the band name');
+    assert
+      .dom('[data-test-rr="band-list-item"]:last-child')
+      .hasText(
+        'Long Distance Calling',
+        'The other band link contains the band name'
+      );
   });
+
+  test('Create a band', async function (assert) {
+    this.server.create('band', { name: 'Royal Blood' });
+
+    await visit('/');
+    await createBand('Caspian');
+    await waitFor('[data-test-rr="no-songs-text"]');
+
+    assert
+      .dom('[data-test-rr="band-list-item"]')
+      .exists({ count: 2 }, 'A new band link is rendered');
+    assert
+      .dom('[data-test-rr="band-list-item"]:last-child')
+      .hasText('Caspian', 'The new bank link is rendered as the last item');
+    assert
+      .dom('[data-test-rr="songs-nav-item"] > .active')
+      .exists('The Songs tab is active');
+  });
+
+  // TODO: Optional exercise - Create a song test case
 });
